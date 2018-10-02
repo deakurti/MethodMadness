76cb3a43affeed8f5719f31dfa97d25d02bf21f1
deakurti <mamibabi7!>
2018-10-02 14:43:40 -0400

    Update 10.2

--- src/DKurtiLib.java
+++ src/DKurtiLib.java
@@ -1,2 +1,26 @@
 public class DKurtiLib {
+
+    public static boolean isPalindrome(String str) {
+
+        int first = 0;
+        int last = str.length()-1;
+        while (first < last) {
+            if (str.charAt(first) != str.charAt(last)) {
+                return false;
+            }
+            first++;
+            last--;
+        }
+        return true;
+
+    }
+    public static boolean dateStr (String str1) {
+
+        str dd = str1.substring (int 3 , int 5);
+        str mm = str1.substring (int 0 , int 2);
+        str yyyy = str1.substring (int 6 , int 10);
+        return ("mm" - "dd" - "yyyy")
+
+    }
+
 }
--- src/TestFile.java
+++ src/TestFile.java
@@ -1,2 +1,8 @@
 public class TestFile {
+    public static void main (String[] args)
+    {
+        System.out.println(DKurtiLib.isPalindrome("Dea"));
+        System.out.println(DKurtiLib.isPalindrome("racecar"));
+    }
 }
+
